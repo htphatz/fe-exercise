77e242dd9a0a2f901e278e69f4cb28e9fdf13126
htphatz <huynhphatsn@gmail.com>
2025-05-06 13:09:14 +0700

    Add user detail component

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -6,6 +8,21 @@ import { UserService } from 'src/app/services/user/user.service';
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css'],
 })
-export class UserDetailComponent {
-  constructor(private userService: UserService) {}
+export class UserDetailComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  user: User = {};
+  id: string = '';
+
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id') || '';
+    this.userService.getById(this.id).subscribe((response) => {
+      if (response.result) {
+        this.user = response.result;
+      }
+    });
+  }
 }
